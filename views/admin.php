d0eb85a57a583daedd7cac13c34476848d9bb1a0
Mte90 <mte90smanettator@gmail.com>
2013-07-17 12:52:29 +0200

    nuovo codice js in admin.php

--- views/admin.php
+++ views/admin.php
@@ -27,18 +27,30 @@
 			//Se già autorizzato non farà niente, mosrà solo Autorizzato da quag
 			echo '<script type="text/javascript" >
 				jQuery(document).ready(function($) {
-				//Al click sul pulsante avvia la chiamata ajax
-					$(\'#quag_ok\').click(function() {
+					function ricerca() {
 						var data = {
 							action: \'quag_search\',
 							search: $(\'#quag_search\').val()
 						};
-
+						$(\'#quag\').html("Ricerca in corso...");
 						$.post(ajaxurl, data, function(response) {
 							$(\'#quag\').html(response)
 						}).fail(function(){
 							alert("error");
 						});
+					}
+					//Al click sul pulsante avvia la chiamata ajax
+						$(\'#quag_ok\').click(function() {
+							ricerca();
+						});
+					//Se premo invio e ho il focus sul campo di ricerca avvia la chiamata ajax
+					$(\'#quag_search\').keypress(function(e){
+						if (e.which == 13 || e.keyCode == 13) {
+							ricerca();
+							e.preventDefault();
+							e.stopPropagation(); 
+							return false;
+						}
 					});
 				});
 				</script>';
