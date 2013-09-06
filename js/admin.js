0b7e61b166d6d535135bc37c629e0d1405d78c3f
Mte90 <mte90smanettator@gmail.com>
2013-09-06 23:51:31 +0200

    add action for dashboard

--- js/admin.js
+++ js/admin.js
@@ -1,32 +1,49 @@
 jQuery(document).ready(function($) {
-	function ricerca() {
+	function ricerca(action) {
 		var data = {
-			action: 'quag_search',
+			action: 'quag_' + action,
 			search: $('#quag_search').val()
 		};
 		$('#quag').html("Ricerca in corso...");
 		$.post(ajaxurl, data, function(response) {
+            if(response.charAt( response.length-1 ) == '0') {
+                response = response.slice(0, -1);
+            }
 			$('#quag').html(response)
 		}).fail(function(){
 			alert("error");
 		});
 	}
+    
 	//Al click sul pulsante avvia la chiamata ajax
 	$('#quag_ok').click(function() {
-		ricerca();
-});
+		ricerca('search');
+    });
+    $('#quag_ok_dashboard').click(function() {
+		ricerca('search_dashboard');
+    });
+    
 	//Se premo invio e ho il focus sul campo di ricerca avvia la chiamata ajax
 	$('#quag_search').keypress(function(e){
 		if (e.which == 13 || e.keyCode == 13) {
-			ricerca();
+			ricerca('search');
 			e.preventDefault();
 			e.stopPropagation(); 
 			return false;
 		}
-		});
+	});
+    $('#quag_search_dashboard').keypress(function(e){
+		if (e.which == 13 || e.keyCode == 13) {
+			ricerca('search_dashboard');
+			e.preventDefault();
+			e.stopPropagation(); 
+			return false;
+		}
+	});
+    
 	//Se clicca il tag avvia la ricerca
 	$('.tag_top').click(function() {
 		$('#quag_search').val($(this).data('name'));
-		ricerca();
+		ricerca('search');
 		});
 }); 
